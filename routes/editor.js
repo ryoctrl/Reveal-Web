b1caad552e2f9ab3a9f701cbff5418d31aa7fe15
mosin <mosin@mosin.jp>
2018-10-20 13:34:49 +0900

    [butfix:1] リファクタリング時の変数名変更漏れを修正

--- routes/editor.js
+++ routes/editor.js
@@ -25,12 +25,12 @@ router.get('/', async function(req, res, next) {
     //clientのvueオブジェクトに渡す際にテンプレートエンジンでレンダリングしなければならないので色々replace。
     //レンダリングしなくても直接vueに渡せる手法があるのであればそっちのがいい。
     //express-vueとかそれっぽい？
-    markdownString = markdownString.replace(new RegExp('`', 'g'), '\\`');
+    markdownString = markdownString.replace(new RegExp('"', 'g'), '\\"');
     //これもレンダリング時に</script>を読むとそこでclientのscriptが終わってしまうので苦肉の策。
     markdownString = markdownString.replace(new RegExp('</script>', 'g'), '<//script>');
 
     let obj = {
-        data: str
+        data: markdownString
     }
     res.render('editor', obj);
 });
